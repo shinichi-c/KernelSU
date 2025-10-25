c567d0a31f279dbe029255ceaf530e33b52ad6bd
kudo <Gokus12456@gmail.com>
2025-10-25 15:51:34 +0530

    Add expected size and hash for KUDO KernelSU

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -1,6 +1,10 @@
 #ifndef __KSU_H_MANAGER_SIGN
 #define __KSU_H_MANAGER_SIGN
 
+// kudo/KernelSU
+#define EXPECTED_SIZE_KUDO      0x3b2
+#define EXPECTED_HASH_KUDO      "eddf58fe3f852f4941f7a7b89367cb992fc3ec96f94ae6e06c028c323fc608e3"
+
 // rsuntk/KernelSU
 #define EXPECTED_SIZE_RSUNTK    0x396
 #define EXPECTED_HASH_RSUNTK    "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"
